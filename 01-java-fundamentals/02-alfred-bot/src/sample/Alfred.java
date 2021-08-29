59bf889501d96a67f5bc5b0bb23fd0b20c4df7ed
Archana <archooworship@gmail.com>
2021-08-29 15:32:49 -0700

    alfred-bot commited

---
+++ 01-java-fundamentals/02-alfred-bot/src/sample/Alfred.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Alfred {
+	
+	public String greeting(String name, String dayPeroidTime) {
+		
+		System.out.println();
+		
+		return name+" "+dayPeroidTime;
+		
+		
+	}
+
+}
---
+++ 01-java-fundamentals/02-alfred-bot/src/sample/AlfredTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class AlfredTest {
+
+	public static void main(String[] args) {
+		
+		Alfred af= new Alfred();
+		System.out.println(af.greeting("Archana", "Good Morning"));
+		
+		
+		
+			
+		
+
+	}
+
+}
