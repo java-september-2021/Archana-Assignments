de2b7880c11c7e066b47babd585d1fc17a0399c2
Archana <archooworship@gmail.com>
2021-09-02 21:34:10 -0700

    alfredBot commited

--- 01-java-fundamentals/02-alfred-bot/src/sample/Alfred.java
+++ 01-java-fundamentals/02-alfred-bot/src/sample/Alfred.java
@@ -1,14 +1,51 @@
 package sample;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Alfred {
 	
-	public String greeting(String name, String dayPeroidTime) {
+	
+	public String basicGreeting() {
 		
-		System.out.println();
+		return "Hello, Lovely to see you. How are you?";
+	}
+	
+	public String guestGreeting(String name, String dayPeroidTime) {
 		
-		return name+" "+dayPeroidTime;
+		return dayPeroidTime+", "+name+" "+" Love to see you.";
 		
 		
 	}
+	
+	public String dateAnnouncement() {
+		
+		DateTimeFormatter date=	DateTimeFormatter.ofPattern("yyyy/mm/dd HH:mm:ss");
+		
+		LocalDateTime now=  LocalDateTime.now();
+		
+		//String date =String.format(, null))
+		
+		return date.format(now); 
+	}
+	
+	
+	
+	public String respondBeforeAlexis(String conversetion ) {
+		
+		if(conversetion.indexOf("Alexis") != -1) {
+		
+			return "Right away, sir.She certainly is not sophisticated enough for that.";
+		
+		} else if (conversetion.indexOf("Alefred") != -1 ){
+		
+			return "At your service. As you wish Naturally.";
+		} else {
+			
+			return "Not found";
+		}
+	
 
+}
+	
 }
--- 01-java-fundamentals/02-alfred-bot/src/sample/AlfredTest.java
+++ 01-java-fundamentals/02-alfred-bot/src/sample/AlfredTest.java
@@ -5,13 +5,30 @@ public class AlfredTest {
 	public static void main(String[] args) {
 		
 		Alfred af= new Alfred();
-		System.out.println(af.greeting("Archana", "Good Morning"));
+		System.out.println(af.guestGreeting("Archana", "Good Morning"));
 		
+		System.out.println(af.dateAnnouncement());
 		
+		String AlexisTest =af.respondBeforeAlexis("Alexis play some low fi beam");
 		
-			
+		//System.out.println( AlexisTest);	
+		String AlefedTest =af.respondBeforeAlexis(" I Can't find yo- yo. May be Alfed may know about it.");
 		
-
+		String notRelaventTest =af.respondBeforeAlexis("May be thats what Batman is all about.Nothing winning but Failing.");
+	
+	
+	
+	System.out.println();
+	System.out.println(AlexisTest);
+	System.out.println(notRelaventTest);
+	System.out.println(AlefedTest);
+	System.out.println();
+	
+	
+	
 	}
 
+	
+	
+	
 }
